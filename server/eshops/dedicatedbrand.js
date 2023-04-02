59eabef4b8fe49a85ebe023c07c3ab0d66d34351
tffnyzhng <tiffanyzheng@hotmail.fr>
2023-04-02 16:00:12 +0200

    feat(shop): scrape new products

--- server/eshops/dedicatedbrand.js
+++ server/eshops/dedicatedbrand.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 /**
  * Parse webpage e-shop
@@ -11,6 +12,7 @@ const parse = data => {
 
   return $('.productList-container .productList')
     .map((i, element) => {
+      const brand = 'dedicated';
       const name = $(element)
         .find('.productList-title')
         .text()
@@ -21,8 +23,17 @@ const parse = data => {
           .find('.productList-price')
           .text()
       );
+      const link ='https://www.dedicatedbrand.com'+ $(element)
+        .find('.productList-link').attr('href');
+      
+      const image =
+        $(element)
+        .find('.js-lazy')
+        .attr('data-src')
 
-      return {name, price};
+      let date = new Date().toISOString().slice(0, 10);
+
+      return {brand,name,link,image,price,date};
     })
     .get();
 };
@@ -50,3 +61,45 @@ module.exports.scrape = async url => {
     return null;
   }
 };
+
+/**
+ * Scrape all the products for a given url page and save as a JSON file
+ * @param  {string}  url
+ * @param  {string}  filename
+ * @return {Promise<boolean>} - true if successful, false otherwise
+ * 
+ */
+
+module.exports.getProducts= async() =>{
+
+  try{ 
+    const response = await fetch ("https://www.dedicatedbrand.com/en/loadfilter");
+    if (response.ok){
+      const body =await response.json();
+      const products = body['products'].filter(
+        data => Object.keys(data).length >0
+      );
+      //console.log(products);
+      data_json =products.map(
+        function(data){
+          const brand = 'dedicated'
+          const image= data['image'][0];
+          const link = "https://www.dedicatedbrand.com/en/"+ data['canonicalUri'];
+          const name =data['name'];
+          const price =data['price']['priceAsNumber'];
+          const date =new Date().toDateString();
+          return{brand,name, link, image, price, date};
+        }
+      );
+       // Write the data to a JSON file
+       fs.writeFileSync('dedicated.json', JSON.stringify(data_json,null, 2));
+
+       return true;
+    }
+    console.error(response);
+    return null;
+  } catch(error){
+    console.error(error);
+    return null;
+  }
+}
